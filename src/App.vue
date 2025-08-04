3d4e96cee5fca9e8196abea3b9394477bcac2f1f
Иван Лихобаба <iv_lix>
2025-08-04 10:53:59 +0300

    Inital repo and first files

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container py-4">
+    <h1 class="mb-4 text-center">Фотолист сотрудников</h1>
+    <PhotoInput />
+    <div class="my-4">
+      <PhotoSheet />
+    </div>
+    <div class="text-center">
+      <SaveButton />
+    </div>
+  </div>
+</template>
